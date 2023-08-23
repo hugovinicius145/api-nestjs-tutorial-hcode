0fafe1cf19b3df45de99a98db026cbbca6cf5e5e
Hugo <hugovinicius145@gmail.com>
2023-08-23 07:53:17 -0300

    Auth Controller Part 1

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
 
 @Module({
   imports: [JwtModule.register({
     secret: `n7di!@04XNJ5HKQeG&uM0SO0tSp!Ej5#`
-  })],
+  }), UserModule],
+  controllers: [AuthController]
 })
 export class AuthModule { }
