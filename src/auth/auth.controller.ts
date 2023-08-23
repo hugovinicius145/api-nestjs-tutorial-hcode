b2e1b36281d785af8615f684ae75c55d81c6d860
Hugo Lima <hugovinicius145@gmail.com>
2023-08-23 10:41:33 -0300

    Assinar Tokens JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,22 +4,32 @@ import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgetDTO } from "./dto/auth-forget.dto";
 import { AuthResetDTO } from "./dto/auth-reset.dto";
 import { UserService } from "src/user/user.service";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) { }
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) { }
 
   @Post('login')
-  async login(@Body() body: AuthLoginDTO) { }
+  async login(@Body() { email, password }: AuthLoginDTO) {
+    return this.authService.login(email, password);
+  }
 
   @Post('register')
   async register(@Body() body: AuthRegisterDTO) {
-    return this.userService.create(body);
+    return this.authService.register(body);
   }
 
   @Post('forget')
-  async forget(@Body() body: AuthForgetDTO) { }
+  async forget(@Body() { email }: AuthForgetDTO) {
+    return this.authService.forget(email);
+  }
 
   @Post('reset')
-  async reset(@Body() body: AuthResetDTO) { }
+  async reset(@Body() { password, token }: AuthResetDTO) {
+    return this.authService.reset(password, token);
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,18 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { UserModule } from "src/user/user.module";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthService } from "./auth.service";
 
 @Module({
-  imports: [JwtModule.register({
-    secret: `n7di!@04XNJ5HKQeG&uM0SO0tSp!Ej5#`
-  }), UserModule],
-  controllers: [AuthController]
+  imports: [
+    JwtModule.register({
+      secret: `n7di!@04XNJ5HKQeG&uM0SO0tSp!Ej5#`
+    }),
+    UserModule,
+    PrismaModule
+  ],
+  controllers: [AuthController],
+  providers: [AuthService]
 })
 export class AuthModule { }
