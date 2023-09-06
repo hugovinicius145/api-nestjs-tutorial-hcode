b0b0207f19210be12d96c39e5ca82ffb22622240
hugo <hugovinicius145@gmail.com>
2023-09-06 16:00:50 -0300

    Variáveis de Ambiente

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthService } from "./auth.service";
 @Module({
   imports: [
     JwtModule.register({
-      secret: `n7di!@04XNJ5HKQeG&uM0SO0tSp!Ej5#`
+      secret: process.env.JWT_SECRET
     }),
     forwardRef(() => UserModule),
     PrismaModule
