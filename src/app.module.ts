d1eebf8d6c69c8311736cf69b334967527f1b1a5
hugo <hugovinicius145@gmail.com>
2023-09-07 10:15:21 -0300

    Mailer Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from "@nestjs-modules/mailer/dist/adapters/pug.adapter"
 
 @Module({
   imports: [
@@ -15,7 +17,20 @@ import { ConfigModule } from '@nestjs/config';
       limit: 100
     }]),
     forwardRef(() => UserModule),
-    forwardRef(() => AuthModule)
+    forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: process.env.SMTP_CONFIG,
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>'
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        }
+      }
+    })
   ],
   controllers: [AppController],
   providers: [AppService, {
